2d302e4fe62cc66d68038e683ef1282fce583a61
PrimkY <fakeprimky@gmail.com>
2021-03-14 12:44:01 +0300

    feat: added some workable alghoritms solutions

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,30 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  const resultArray = [];
+    for (let i = 0; i < arr.length; i += 1) {
+      switch (arr[i]) {
+        case "--discard-prev":
+          resultArray.pop();
+          break;
+        case "--discard-next":
+          resultArray.push("delete");
+          i += 1;
+          break;
+        case "--double-prev":
+          if (i !== 0) {
+            resultArray.push(resultArray[resultArray.length - 1]);
+          }
+          break;
+        case "--double-next":
+          if (i !== arr.length - 1) {
+            resultArray.push(arr[i + 1]);
+          }
+          break;
+        default:
+          resultArray.push(arr[i]);
+          break;
+      }
+    }
+    return resultArray.filter((e) => e !== "delete");
 };
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,14 +1,48 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(expression) {
+    this.expression = expression;
+  }
+    encrypt(message, key) {
+      if (message == null || key == null) throw Error();
+      const firstCode = 'A'.charCodeAt();
+      const latinAlphabet = /[A-Za-z]/;
+      const alphabetLenght = 26;
+      let counter = 0;
+        message = message.toUpperCase().split('');
+        key = key.toUpperCase();
+      message.forEach((element, index) => {
+        if (latinAlphabet.test(element)) {
+          message[index] = String.fromCharCode(
+            ((element.charCodeAt() - firstCode
+              + (key[counter % key.length].charCodeAt() - firstCode))
+              % alphabetLenght)
+            + firstCode);
+          counter += 1;
+        }
+      });
+      return this.expression === false ? message.reverse().join('') : message.join('');
+    }
+    decrypt(message, key) {
+      if (message == null || key == null) throw Error();
+      const latinAlphabet = /[A-Za-z]/;
+      const alphabetLenght = 26;
+      const firstCode = 'A'.charCodeAt();
+      let counter = 0;
+        message = message.toUpperCase().split('');
+        key = key.toUpperCase();
+      message.forEach((element, index) => {
+        if (latinAlphabet.test(element)) {
+          message[index] = String.fromCharCode(
+            ((element.charCodeAt() - firstCode
+              + (alphabetLenght - (key[counter % key.length].charCodeAt() - firstCode)))
+              % alphabetLenght)
+            + firstCode);
+            counter += 1;
+        }
+      });
+      return this.expression === false ? message.reverse().join('') : message.join('');
   }
 }
-
 module.exports = VigenereCipheringMachine;
